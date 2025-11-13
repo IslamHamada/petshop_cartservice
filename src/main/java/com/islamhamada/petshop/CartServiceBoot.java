a3de1f3ed20b977f61521fbb249c9556b5589ed6
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 13:54:36 +0100

    add base Cart service and configs

---
+++ src/main/java/com/islamhamada/petshop/CartServiceBoot.java
@@ -0,0 +1,13 @@
+package com.islamhamada.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceBoot.class, args);
+	}
+
+}
