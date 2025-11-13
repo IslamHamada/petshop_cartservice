23f87a61516760e05b13a2be5d7ff717944a7250
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 17:26:21 +0100

    CartService interface

--- src/main/java/com/islamhamada/petshop/services/CartService.java
+++ src/main/java/com/islamhamada/petshop/services/CartService.java
@@ -1,4 +1,11 @@
 package com.islamhamada.petshop.services;
 
+import com.islamhamada.petshop.entity.Cart;
+import com.islamhamada.petshop.model.AddCartItemRequest;
+
+import java.util.List;
+
 public interface CartService {
+    String addCartItem(AddCartItemRequest request);
+    List<Cart> getUserCart(String user_id);
 }
