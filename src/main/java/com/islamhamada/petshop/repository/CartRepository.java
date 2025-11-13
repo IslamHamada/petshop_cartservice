9df1555e5fa8beb44a58bfa039229453d35bd984
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 17:27:18 +0100

    add JPA find functions

--- src/main/java/com/islamhamada/petshop/repository/CartRepository.java
+++ src/main/java/com/islamhamada/petshop/repository/CartRepository.java
@@ -4,6 +4,11 @@ import com.islamhamada.petshop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserIdAndProductId(String user_id, long product_id);
+    List<Cart> findByUserId(String user_id);
 }
