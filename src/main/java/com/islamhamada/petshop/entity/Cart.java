432504bdfef10d1a7f35ebe11417e330ef355430
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 17:26:06 +0100

    add @Data and @Builder to Cart

--- src/main/java/com/islamhamada/petshop/entity/Cart.java
+++ src/main/java/com/islamhamada/petshop/entity/Cart.java
@@ -3,8 +3,12 @@ package com.islamhamada.petshop.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
+@Data
+@Builder
 public class Cart {
     @Id
     @GeneratedValue
