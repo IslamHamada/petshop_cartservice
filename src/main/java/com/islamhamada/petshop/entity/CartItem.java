f6199b7f19b49afee8dccceb3881d2f232f5e3e7
IslamHamada <Islamhamada222@gmail.com>
2025-11-14 14:32:25 +0100

    add needed lombok annotations

--- src/main/java/com/islamhamada/petshop/entity/CartItem.java
+++ src/main/java/com/islamhamada/petshop/entity/CartItem.java
@@ -3,12 +3,16 @@ package com.islamhamada.petshop.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
     @Id
     @GeneratedValue
