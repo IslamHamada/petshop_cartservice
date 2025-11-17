aa35433ca897658d9063408529e0bfd7861af146
IslamHamada <Islamhamada222@gmail.com>
2025-11-17 22:37:01 +0100

    add @Builder annotation

--- src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
+++ src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
@@ -1,8 +1,10 @@
 package com.islamhamada.petshop.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AddCartItemRequest {
     private long backend_id;
     private long product_id;
