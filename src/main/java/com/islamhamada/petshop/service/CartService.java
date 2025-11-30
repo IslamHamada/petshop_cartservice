d9ab4955569a84473acedc57225a8317f61b3a8e
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:33:02 +0100

    correct import paths

--- src/main/java/com/islamhamada/petshop/service/CartService.java
+++ src/main/java/com/islamhamada/petshop/service/CartService.java
@@ -1,6 +1,6 @@
 package com.islamhamada.petshop.service;
 
-import com.islamhamada.petshop.contracts.ElaborateCartItemDTO;
+import com.islamhamada.petshop.contracts.dto.ElaborateCartItemDTO;
 import com.islamhamada.petshop.model.AddCartItemRequest;
 import com.islamhamada.petshop.model.AddSessionCartRequest;
 
