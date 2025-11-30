c389c10103672cb3278723f8044d003114ee422c
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:31:42 +0100

    add validation annotations

--- src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
+++ src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
@@ -1,12 +1,16 @@
 package com.islamhamada.petshop.model;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class AddCartItemRequest {
+    @Positive
     private long backend_id;
+    @Positive
     private long product_id;
+    @Positive
     private int count;
 }
