56e2943052ca0c71b590bc90f714156e41a1d92c
IslamHamada <Islamhamada222@gmail.com>
2025-12-03 01:21:16 +0100

    add validation to the RestController

--- src/main/java/com/islamhamada/petshop/controller/CartController.java
+++ src/main/java/com/islamhamada/petshop/controller/CartController.java
@@ -5,6 +5,8 @@ import com.islamhamada.petshop.model.AddCartItemRequest;
 import com.islamhamada.petshop.model.AddSessionCartRequest;
 import com.islamhamada.petshop.model.UpdateCartItemCountRequest;
 import com.islamhamada.petshop.service.CartService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,49 +24,49 @@ public class CartController {
 
     @PreAuthorize("hasAnyRole('Customer')")
     @PostMapping
-    public ResponseEntity<Long> addCartItem(@RequestBody AddCartItemRequest request){
+    public ResponseEntity<Long> addCartItem(@Valid @RequestBody AddCartItemRequest request){
         long rv = cartService.addCartItem(request);
         return new ResponseEntity<>(rv, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @GetMapping("/{user_id}")
-    public ResponseEntity<List<ElaborateCartItemDTO>> getCartByUser(@PathVariable long user_id){
+    public ResponseEntity<List<ElaborateCartItemDTO>> getCartByUser(@Positive @PathVariable long user_id){
         List<ElaborateCartItemDTO> user_cart = cartService.getUserCart(user_id);
         return new ResponseEntity<>(user_cart, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @DeleteMapping("/{user_id}")
-    public ResponseEntity<Long> emptyCartOfUser(@PathVariable long user_id){
+    public ResponseEntity<Long> emptyCartOfUser(@Positive @PathVariable long user_id){
         long deletedRows = cartService.emptyCartOfUser(user_id);
         return new ResponseEntity<>(deletedRows, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @PutMapping("/{cart_item_id}")
-    public ResponseEntity<Integer> updateCartItemCount(@PathVariable long cart_item_id, @RequestBody UpdateCartItemCountRequest request) {
+    public ResponseEntity<Integer> updateCartItemCount(@Positive @PathVariable long cart_item_id, @Valid @RequestBody UpdateCartItemCountRequest request) {
         int count = cartService.updateCartItemCount(cart_item_id, request.getCount());
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @DeleteMapping("/delete/{cart_item_id}")
-    public ResponseEntity deleteCartItem(@PathVariable long cart_item_id) {
+    public ResponseEntity deleteCartItem(@Positive @PathVariable long cart_item_id) {
         cartService.deleteCartItem(cart_item_id);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @GetMapping("/item_count/{user_id}")
-    public ResponseEntity<Integer> getCartItemCount(@PathVariable long user_id) {
+    public ResponseEntity<Integer> getCartItemCount(@Positive @PathVariable long user_id) {
         int count = cartService.getCartItemCount(user_id);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
     @PostMapping("/login_to_checkout/{user_id}")
-    public ResponseEntity addSessionCart(@PathVariable int user_id, @RequestBody AddSessionCartRequest request){
+    public ResponseEntity addSessionCart(@Positive @PathVariable int user_id, @Valid @RequestBody AddSessionCartRequest request){
         cartService.addSessionCart(user_id, request);
         return new ResponseEntity(HttpStatus.OK);
     }
