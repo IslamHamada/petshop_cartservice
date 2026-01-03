1325bb80bc9abbe5492b576ad1413529081ff5b1
IslamHamada <islamhamada222@gmail.com>
2026-01-03 18:13:01 +0100

    separate feign client name from path

--- src/main/java/com/islamhamada/petshop/external/service/ProductService.java
+++ src/main/java/com/islamhamada/petshop/external/service/ProductService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ProductService/product")
+@FeignClient(name = "ProductService", path = "/product")
 public interface ProductService {
     @GetMapping("/{id}")
     ResponseEntity<ProductDTO> getProductById(@PathVariable("id") long id);
