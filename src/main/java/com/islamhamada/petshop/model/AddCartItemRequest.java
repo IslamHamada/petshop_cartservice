7c92b0bf9d4a6a21d2e4423a8bd4136a71a3ca2f
IslamHamada <islamhamada222@gmail.com>
2026-01-03 18:13:33 +0100

    use @PositiveOrZero instead of @Positive

--- src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
+++ src/main/java/com/islamhamada/petshop/model/AddCartItemRequest.java
@@ -1,15 +1,16 @@
 package com.islamhamada.petshop.model;
 
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class AddCartItemRequest {
-    @Positive
+    @PositiveOrZero
     private long backend_id;
-    @Positive
+    @PositiveOrZero
     private long product_id;
     @Positive
     private int count;
