d6b80237808d06c3b6c29d0249140b4958afd39e
IslamHamada <islamhamada222@gmail.com>
2026-01-14 20:04:29 +0100

    Use WireMockServer instead of WireMockExtension

--- src/test/java/com/islamhamada/petshop/TestServiceInstanceListSupplier.java
+++ src/test/java/com/islamhamada/petshop/TestServiceInstanceListSupplier.java
@@ -9,6 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestServiceInstanceListSupplier implements ServiceInstanceListSupplier {
+
+    private final int port;
+
+    public TestServiceInstanceListSupplier(int port) {
+        this.port = port;
+    }
+
     @Override
     public String getServiceId() {
         return "ProductService";
@@ -22,7 +29,7 @@ public class TestServiceInstanceListSupplier implements ServiceInstanceListSuppl
                 "PRODUCT-SERVICE",
                 "ProductService",
                 "localhost",
-                8080,
+                port,
                 false
         ));
         return Flux.just(list);
